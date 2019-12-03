a5579e89a62b47ac55a13e1b13305707d32a7a2e
Igor <Igor@MSI.gateway.sasktel.net>
2019-12-02 21:22:22 -0600

    add fixes to HelloBugs.java

--- HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
+++ HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
@@ -1,11 +1,11 @@
 package com.openclassrooms;
 
-public class HelloBugs {
+public clas HelloBugs {
 
-	public static void main(String[] args) {
+	public static void main(String{}args) {
 		
-		for(int i=0;i<5;i++) {
-		System.out.println("Hello buggy code!");
+		for(i=0;i>=0;i++) {
+		System.out.println("Hello buggy code!"):
 		}
 				
 	}
