23da004d0e838dd43a1d1f2691fc41b7bb6e7512
bertrandnicolas <bertrandnicolas@192.168.1.5>
2021-11-13 15:29:10 +0100

    add fixes to HelloBugs.java for issues 1 and 2

--- HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
+++ HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
@@ -1,13 +1,9 @@
 package com.openclassrooms;
 
 public class HelloBugs {
-
 	public static void main(String[] args) {
-		
 		for(int i=0; i<5; i++) {
 			System.out.println("Hello buggy code!");
 		}
-				
 	}
-
 }
