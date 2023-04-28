eecb8a83348f3124c819c325d038043aa6b3d8cd
Mr. Big <Mr. Big@DESKTOP-ATBS78F>
2023-04-28 07:42:21 -0700

    add fixes to HelloBugs.java for issues 1 & 2

--- HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
+++ HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
@@ -3,8 +3,8 @@ package com.openclassrooms;
 public class HelloBugs {
 
 	public static void main(String[] args) {
-		
-		for(int i=0;i<3;i++) {
+		int i;
+		for(i=0;i<3;i++) {
 		System.out.println("Hello buggy code!");
 		}
 				
