c6c7d4e78feef94d1b4db3fe1633c48242c2bf8b
geraldy1st <geraldy.leondas@gmail.com>
2023-10-25 12:09:31 -0400

    loop fixed

--- HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
+++ HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
@@ -4,7 +4,7 @@ public class HelloBugs {
 
 	public static void main(String[]args) {
 		
-		for ( int i=0 ;i<5; i++) {
+		for ( int i=0 ;i<=4; i++) {
 		System.out.println("Hello buggy code! " + "iteration: " + i );
 		}
 				
